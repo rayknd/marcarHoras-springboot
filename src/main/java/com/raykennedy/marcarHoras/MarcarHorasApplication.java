87506a7861fda8fc30fd4b81bc557879135d370b
Ray Kennedy <ray.knd@gmail.com>
2021-03-22 17:48:38 -0300

    Criação do projeto

---
+++ src/main/java/com/raykennedy/marcarHoras/MarcarHorasApplication.java
@@ -0,0 +1,13 @@
+package com.raykennedy.marcarHoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcarHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcarHorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raykennedy/marcarHoras/MarcarHorasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raykennedy.marcarHoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcarHorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
